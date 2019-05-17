48dd0b7e6b62eadc9b09accae57430cfb9da57aa
ydarma <yves.darmaillac@gmail.com>
2019-05-17 11:26:52 +0200

    naming

--- mcmc/algo.go
+++ mcmc/algo.go
@@ -3,15 +3,15 @@ package mcmc
 import "distclus/core"
 
 // NewAlgo creates a new kmeans algo
-func NewAlgo(mcmcConf Conf, space core.Space, data []core.Elemt, initializer core.Initializer, args ...interface{}) *core.Algo {
-	SetConfigDefaults(&mcmcConf)
-	Verify(mcmcConf)
-	var distrib = hanfleArgs(args)
-	var impl = getImpl(mcmcConf, initializer, data, distrib)
-	return buildAlgo(mcmcConf, impl, space)
+func NewAlgo(conf Conf, space core.Space, data []core.Elemt, initializer core.Initializer, args ...interface{}) *core.Algo {
+	SetConfigDefaults(&conf)
+	Verify(conf)
+	var distrib = handleArgs(args)
+	var impl = getImpl(conf, initializer, data, distrib)
+	return buildAlgo(conf, impl, space)
 }
 
-func hanfleArgs(args []interface{}) (distrib Distrib) {
+func handleArgs(args []interface{}) (distrib Distrib) {
 	if len(args) == 1 {
 		distrib = args[0].(Distrib)
 	}
